b78e4863c5780e5509bb5b767a96d06c97e64b1d
Arthur <nod888@mail.ru>
2023-12-18 13:24:04 +0300

    Changed feedback webapp

--- text/button_text.py
+++ text/button_text.py
@@ -1,7 +1,7 @@
 
 #web_app_reg = 'https://form-city-day-night.vercel.app'
 web_app_menu = 'https://city-day-night.ru/'
-web_app_feedback = 'https://nextjs-test-lzmm-fsn85redh-tw1ster4k.vercel.app/reviews'
+web_app_feedback = 'https://city-day-night.ru/reviews'
 contact_photo_id = 'AgACAgIAAxkBAAILKmVeQvZOcpRcw4mJJuIYwbMgMxc_AAJI3DEb937wSmEyx8fT89GDAQADAgADeAADMwQ'
 
 
